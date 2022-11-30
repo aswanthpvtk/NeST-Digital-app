3894352acf6428691481d7a611c855d0e09b3b59
Aswanth P <Aswanth@NTP-LAP-734.chn.nestgroup.net>
2022-11-30 14:05:18 +0530

    nest

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AdminLoginComponent } from './admin-login/admin-login.component';
 import { FormsModule } from '@angular/forms';
 import { EmpLoginComponent } from './emp-login/emp-login.component';
 import { EmpRegComponent } from './emp-reg/emp-reg.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AdminLoginComponent,
     EmpLoginComponent,
-    EmpRegComponent
+    EmpRegComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
